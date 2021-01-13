25bbca1492db4af23c4930e2af94b25a43d92072
Jaime Reuss <jamesreuss1@gmail.com>
2021-01-13 20:29:54 +0000

    moved connection function to client.js and named my snek

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: `135.23.222.131`,
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', function() {
+    console.log('successfully connected to server');
+    conn.write("Name: JAR")
+  });
+
+  conn.on('data', function(message) {
+    console.log(message);
+  });
+
+  return conn;
+};
+
+module.exports = {connect};
